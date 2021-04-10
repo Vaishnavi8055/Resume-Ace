ed3caf2716d504f7bf0fa4fb54ab61de7d537e1c
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2021-04-11 00:35:43 +0530

    add addNewAQField function

--- HTML Files/CVTemplates/1/temp.js
+++ HTML Files/CVTemplates/1/temp.js
@@ -10,3 +10,16 @@ function addnewWEField() {
     let weAddButtonOb = document.getElementById("weAddButton")
     weOb.insertBefore(newNode, weAddButtonOb)
 }
+
+function addNewAQField() {
+    let newNode = document.createElement("textarea")
+    newNode.classList.add("form-control")
+    newNode.classList.add("aqField")
+    newNode.classList.add("mt-2")
+    newNode.setAttribute("rows", 3)
+    newNode.setAttribute("placeholder", 'Enter here')
+
+    let aqOb = document.getElementById("aq")
+    let aqAddButtonOb = document.getElementById("aqAddButton")
+    aqOb.insertBefore(newNode, aqAddButtonOb)
+}
