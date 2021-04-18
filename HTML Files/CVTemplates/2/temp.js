4b0d5da71a73791485d00d909559663949ac3842
Pratibha499 <pratibha.sharma_cs18@gla.ac.in>
2021-04-18 15:42:23 +0530

    addnewfield function added

---
+++ HTML Files/CVTemplates/2/temp.js
@@ -0,0 +1,12 @@
+function addnewWEField() {
+    let newNode = document.createElement("textarea")
+    newNode.classList.add("form-control")
+    newNode.classList.add("weField")
+    newNode.classList.add("mt-2")
+    newNode.setAttribute("rows", 3)
+    newNode.setAttribute("placeholder", 'Enter here')
+
+    let weOb = document.getElementById("we")
+    let weAddButtonOb = document.getElementById("weAddButton")
+    weOb.insertBefore(newNode, weAddButtonOb)
+}
