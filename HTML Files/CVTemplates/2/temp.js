2662c23a1044840b5eaeb12fdad2a91177a5388c
Pratibha499 <pratibha.sharma_cs18@gla.ac.in>
2021-04-18 15:45:59 +0530

    generateCV code added

--- HTML Files/CVTemplates/2/temp.js
+++ HTML Files/CVTemplates/2/temp.js
@@ -25,5 +25,51 @@ function addNewAQField() {
 }
 
 function generateCV() {
+    let nameField = document.getElementById("nameField").value;
+    let nameT1 = document.getElementById("nameT1");
+    let nameT2 = document.getElementById("nameT2");
+    nameT1.innerHTML = nameField;
+    nameT2.innerHTML = nameField;
+
+    //direct
+    //contact
+    document.getElementById("contactT").innerHTML = document.getElementById("contactField").value;
+    //address
+    document.getElementById("addressT").innerHTML = document.getElementById("addressField").value;
+    //social links
+    document.getElementById("fbT").innerHTML = document.getElementById("fbField").value;
+    document.getElementById("instaT").innerHTML = document.getElementById("instaField").value;
+    document.getElementById("linkedT").innerHTML = document.getElementById("linkedField").value;
+    //objective
+    document.getElementById("objectiveT").innerHTML = document.getElementById("objectiveField").value;
+
+    //work experience
+    let wes = document.getElementsByClassName('weField')
+    let str = "";
+    for (let e of wes) {
+        str = str + `<li> ${e.value} </li>`;
+    }
+    document.getElementById('weT').innerHTML = str;
+
+    //Academic Qualification
+    let aqs = document.getElementsByClassName('aqField')
+    let str1 = "";
+    for (let e1 of aqs) {
+        str1 = str1 + `<li> ${e1.value} </li>`;
+    }
+    document.getElementById('aqT').innerHTML = str1;
+
+    //profile picture
+    let file = document.getElementById("imageField").files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    //set the image to template
+    reader.onloadend = function() {
+        document.getElementById("imageT").src = reader.result;
+    }
+
+    document.getElementById("cv-form1").style.display = 'none';
+    document.getElementById("cv-form2").style.display = 'none';
+    document.getElementById("cv-template1").style.display = 'block';
 
 }
